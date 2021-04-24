d8ff4ef835e1e3b4b6138145f7b7bb177207f765
Mou <m.mounir.f@gmail.com>
2021-04-25 00:40:31 +0200

    :art: Introducing Stepper component
    to show one question at a time

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import FormQuestion from "./components/FormQuestion/FormQuestion";
 import './App.scss';
 import { useEffect } from 'react';
 import { IForm } from './interfaces/Form';
+import Stepper from './components/Stepper/Stepper';
+import { useAppSelector } from './hooks/useAppSelector';
 
 const initialFormState = {
   firstName: "",
@@ -14,6 +16,7 @@ const initialFormState = {
 };
 
 function App() {
+  const currentStep = useAppSelector((state) => state.stepper.currentStep);
   const [formState, setFormState] = useState<IForm>(initialFormState);
   const onQuestionChange = ( questionId: string, event: React.ChangeEvent<HTMLInputElement> ) => {
     const { value } = event.target;
@@ -23,21 +26,23 @@ function App() {
   useEffect(() => {
     console.log(formState)
   }, [formState])
+
+  const formSteps = questions.map(({ id, title, answer }) => (
+    <FormQuestion
+      key={id}
+      id={id}
+      title={title}
+      answer={answer}
+      onChange={onQuestionChange}
+      value={formState[id as keyof IForm]}
+    />
+  ));
   
   return (
     <div className="App">
-      {
-        questions.map(({ id, title, answer }) => (
-          <FormQuestion
-            key={id}
-            id={id}
-            title={title}
-            answer={answer}
-            onChange={onQuestionChange}
-            value={formState[id as keyof IForm]}
-          />
-        ))
-      }
+      <Stepper currentStep={currentStep}>
+        {formSteps}
+      </Stepper>
     </div>
   );
 }
---
+++ src/components/Stepper/Stepper.tsx
@@ -0,0 +1,35 @@
+
+import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import { SetCurrentStep } from '../../slices/StepperSlice';
+
+interface StepperProps {
+    currentStep: number;
+    children: Array<JSX.Element>;
+}
+
+const Stepper: React.FunctionComponent<StepperProps> = ({ currentStep, children }) => {
+    const dispatch = useAppDispatch();
+    const questions = useAppSelector((state) => state.stepper.questions);
+    const isFirst = useAppSelector((state) => state.stepper.isFirst);
+    const isLast = useAppSelector((state) => state.stepper.isLast);
+    const nextStep = currentStep + 1;
+    const prevStep = currentStep - 1;
+
+    return (
+        <div className="stepper">
+            <div className="header">
+                <p className="header__info">Question <strong>{currentStep}</strong> of <strong>{questions.length}</strong></p>
+                <div className="header__navigation">
+                    <button className="nav-button" disabled={isFirst} onClick={() => dispatch(SetCurrentStep({currentStep: prevStep}))}>Previous</button>
+                    <button className="nav-button" disabled={isLast} onClick={() => dispatch(SetCurrentStep({currentStep: nextStep}))}>Next</button>
+                </div>
+            </div>
+            <div className="stepper__content">
+                {children[currentStep - 1  ]}
+            </div>
+        </div>
+    )
+}
+
+export default Stepper;
